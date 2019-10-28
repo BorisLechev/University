4b7eb1eb121d9ee610668ebe0115bf0a80f8a718
BorisLechev <b.k.lechev@gmail.com>
2019-10-29 00:06:38 +0200

    Added Minimum Transportation Cost Calculator.

---
+++ C#/Coursework/Coursework/Calculator.Designer.cs
@@ -0,0 +1,338 @@
+﻿namespace Coursework
+{
+    partial class Calculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.textBox13 = new System.Windows.Forms.TextBox();
+            this.textBox14 = new System.Windows.Forms.TextBox();
+            this.textBox15 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox16 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(256, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(43, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(316, 45);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(44, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(376, 45);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(43, 20);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(434, 45);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(43, 20);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(256, 71);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(43, 20);
+            this.textBox5.TabIndex = 4;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(316, 71);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(44, 20);
+            this.textBox6.TabIndex = 5;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(376, 71);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(43, 20);
+            this.textBox7.TabIndex = 6;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(434, 71);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(43, 20);
+            this.textBox8.TabIndex = 7;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(256, 97);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(43, 20);
+            this.textBox9.TabIndex = 8;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(316, 97);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(44, 20);
+            this.textBox10.TabIndex = 9;
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(376, 97);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(43, 20);
+            this.textBox11.TabIndex = 10;
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(434, 97);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(43, 20);
+            this.textBox12.TabIndex = 11;
+            // 
+            // textBox13
+            // 
+            this.textBox13.Location = new System.Drawing.Point(256, 123);
+            this.textBox13.Name = "textBox13";
+            this.textBox13.Size = new System.Drawing.Size(43, 20);
+            this.textBox13.TabIndex = 12;
+            // 
+            // textBox14
+            // 
+            this.textBox14.Location = new System.Drawing.Point(316, 123);
+            this.textBox14.Name = "textBox14";
+            this.textBox14.Size = new System.Drawing.Size(44, 20);
+            this.textBox14.TabIndex = 13;
+            // 
+            // textBox15
+            // 
+            this.textBox15.Location = new System.Drawing.Point(376, 123);
+            this.textBox15.Name = "textBox15";
+            this.textBox15.Size = new System.Drawing.Size(43, 20);
+            this.textBox15.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(438, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Supply";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(187, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Demand";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(262, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 13);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "D1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(327, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(21, 13);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "D2";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(387, 20);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(21, 13);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "D3";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(201, 45);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(20, 13);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "S1";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(201, 74);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(20, 13);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "S2";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(201, 100);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(20, 13);
+            this.label8.TabIndex = 23;
+            this.label8.Text = "S3";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.HotPink;
+            this.button1.Location = new System.Drawing.Point(256, 161);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(221, 30);
+            this.button1.TabIndex = 24;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(335, 206);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(61, 22);
+            this.label9.TabIndex = 25;
+            this.label9.Text = "Result";
+            // 
+            // textBox16
+            // 
+            this.textBox16.Location = new System.Drawing.Point(256, 241);
+            this.textBox16.Multiline = true;
+            this.textBox16.Name = "textBox16";
+            this.textBox16.Size = new System.Drawing.Size(221, 58);
+            this.textBox16.TabIndex = 26;
+            // 
+            // Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox16);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox15);
+            this.Controls.Add(this.textBox14);
+            this.Controls.Add(this.textBox13);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Calculator";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Calculator_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.TextBox textBox13;
+        private System.Windows.Forms.TextBox textBox14;
+        private System.Windows.Forms.TextBox textBox15;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox16;
+    }
+}
+
---
+++ C#/Coursework/Coursework/Calculator.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Coursework
+{
+    public partial class Calculator : Form
+    {
+        public string[,] secondMatrix;
+
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void Calculator_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int firstRowFirstCol = int.Parse(textBox1.Text);
+            int firstRowSecondCol = int.Parse(textBox2.Text);
+            int firstRowThirdCol = int.Parse(textBox3.Text);
+            int firstRowFourthCol = int.Parse(textBox4.Text);
+            int secondRowFirstCol = int.Parse(textBox5.Text);
+            int secondRowSecondCol = int.Parse(textBox6.Text);
+            int secondRowThirdCol = int.Parse(textBox7.Text);
+            int secondRowFourthCol = int.Parse(textBox8.Text);
+            int thirdRowFirstCol = int.Parse(textBox9.Text);
+            int thirdRowSecondCol = int.Parse(textBox10.Text);
+            int thirdRowThirdCol = int.Parse(textBox11.Text);
+            int thirdRowFourthCol = int.Parse(textBox12.Text);
+            int fourthRowFirstCol = int.Parse(textBox13.Text);
+            int fourthRowSecondCol = int.Parse(textBox14.Text);
+            int fourthRowThirdCol = int.Parse(textBox15.Text);
+            int fourthRowFourthCol = 0;
+
+            int[,] firstMatrix = new int[,]
+            {
+                { firstRowFirstCol, firstRowSecondCol, firstRowThirdCol, firstRowFourthCol },
+                { secondRowFirstCol, secondRowSecondCol, secondRowThirdCol, secondRowFourthCol },
+                { thirdRowFirstCol, thirdRowSecondCol, thirdRowThirdCol, thirdRowFourthCol },
+                { fourthRowFirstCol, fourthRowSecondCol, fourthRowThirdCol, fourthRowFourthCol }
+            };
+
+            this.secondMatrix = new string[,]
+            {
+                { "-", "-", "-", "-" },
+                { "-", "-", "-", "-" },
+                { "-", "-", "-", "-" },
+                { "-", "-", "-", "-" }
+            };
+
+            int row = Int32.MinValue;
+            int col = Int32.MinValue;
+            List<int> coordinates = new List<int>();
+
+            do
+            {
+                coordinates = FindTheCoordinatesOfTheMinNumber(firstMatrix);
+                row = coordinates[0];
+                col = coordinates[1];
+
+                if (row < 0 && col < 0)
+                {
+                    break;
+                }
+
+                PutTheMinNumberInTheMatrix(firstMatrix, row, col);
+            } while (coordinates.Count == 2);
+
+            textBox16.Text = PrintResult(firstMatrix);
+        }
+
+        private List<int> FindTheCoordinatesOfTheMinNumber(int[,] matrix)
+        {
+            List<int> coordinates = new List<int>();
+            int finalRow = Int32.MinValue;
+            int finalCol = Int32.MinValue;
+            int minValue = Int32.MaxValue;
+
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    int currentNumber = matrix[row, col];
+
+                    if (this.secondMatrix[row, col] != "-" || matrix[3, col + 1] < 0 || matrix[row + 1, 3] < 0 || (row == 3 && col == 3))
+                    {
+                        continue;
+                    }
+
+                    if (currentNumber < minValue)
+                    {
+                        minValue = currentNumber;
+                        finalRow = row;
+                        finalCol = col;
+                    }
+                }
+            }
+
+            coordinates.Add(finalRow);
+            coordinates.Add(finalCol);
+
+            return coordinates;
+        }
+
+        private void PutTheMinNumberInTheMatrix(int[,] matrix, int row, int col)
+        {
+            int minNumber = Math.Min(matrix[3, col], matrix[row, 3]);
+
+            this.secondMatrix[row, col] = minNumber.ToString();
+
+            matrix[3, col] -= minNumber;
+            matrix[row, 3] -= minNumber;
+        }
+
+        private string PrintResult(int[,] firstMatrix)
+        {
+            string result = "";
+            int value = 0;
+
+            for (int row = 0; row < firstMatrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < firstMatrix.GetLength(1); col++)
+                {
+                    if (this.secondMatrix[row, col] == "-")
+                    {
+                        continue;
+                    }
+
+                    int currentNumberFirstMatrix = firstMatrix[row, col];
+                    int currentNumberSecondMatrix = int.Parse(this.secondMatrix[row, col]);
+
+                    if (currentNumberSecondMatrix <= 0)
+                    {
+                        continue;
+                    }
+
+                    value += currentNumberFirstMatrix * currentNumberSecondMatrix;
+                    result += $"({currentNumberFirstMatrix} x {currentNumberSecondMatrix}) + ";
+                }
+            }
+
+            result = result.Substring(0, result.Length - 3);
+            result += $" = {value}";
+
+            return result;
+        }
+    }
+}
