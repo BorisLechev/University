d1d5cfd827e122f2c4a49dd65645fbe73593b083
BorisLechev <b.k.lechev@gmail.com>
2020-10-16 23:59:37 +0300

    Add scroll down arrow

--- Human-Computer Interaction/Pets/src/app/components/home/home.component.ts
+++ Human-Computer Interaction/Pets/src/app/components/home/home.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import * as $ from 'jquery';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
   constructor() { }
+
+  ngOnInit(): void {
+    $("a[href^='#']").click(function (event){
+      event.preventDefault();
+
+      let target = $($(this).attr("href"));
+
+      if (target.length){
+        $("html,body").animate({scrollTop:target.offset().top}, 600);
+      }
+    });
+    // let linkTag = document.getElementById("scrollDownArrow");
+    // linkTag.addEventListener("click", function (e) {
+    //   e.preventDefault();
+    //   let linkUrl = linkTag.getAttribute("href").replace("#", "");
+    //   let destinationOffsetTop = document.getElementById(linkUrl).offsetTop;
+
+    //   // if (linkUrl.length) {
+    //     document.querySelectorAll("html, body")[0].animate({ scrollTop: destinationOffsetTop });
+    //   // }
+    // });
+  }
 }
