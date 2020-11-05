753f9140ac0fb7ef3515184c145a40f75649561d
BorisLechev <b.k.lechev@gmail.com>
2020-11-05 11:46:04 +0200

    Fix opening of dogs and cats section

--- Human-Computer Interaction/Pets/src/app/app-routing.module.ts
+++ Human-Computer Interaction/Pets/src/app/app-routing.module.ts
@@ -1,21 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ArticleComponent } from './components/articles/article/article.component';
 import { ArticlesSectionComponent } from './components/articles/articles-section/articles-section.component';
 import { ErrorPageComponent } from './components/error-page/error-page.component';
 import { HomeComponent } from './components/home/home.component';
-import { ListOfPetSpeciesComponent } from './components/list-of-pet-species/list-of-pet-species.component';
 import { PetShopsSectionComponent } from './components/pet-shops/pet-shops-section/pet-shops-section.component';
+import { ListOfPetSpeciesComponent } from './components/pets/list-of-pet-species/list-of-pet-species.component';
 import { PetDetailsComponent } from './components/pets/pet-details/pet-details.component';
 import { PetsSectionComponent } from './components/pets/pets-section/pets-section.component';
-import { VeterinarianCardComponent } from './components/veterinarians/veterinarian-card/veterinarian-card.component';
 import { VeterinariansSectionComponent } from './components/veterinarians/veterinarians-section/veterinarians-section.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '', component: HomeComponent },
   { path: 'articles', component: ArticlesSectionComponent },
-  { path: 'species', component: ListOfPetSpeciesComponent },
+  { path: 'pet/species', component: ListOfPetSpeciesComponent },
   { path: 'pets/:type', component: PetsSectionComponent},
   { path: 'pet/:id', component: PetDetailsComponent },
   { path: "veterinarians", component: VeterinariansSectionComponent },
--- Human-Computer Interaction/Pets/src/app/app.module.ts
+++ Human-Computer Interaction/Pets/src/app/app.module.ts
@@ -7,10 +7,6 @@ import { HomeComponent } from './components/home/home.component';
 import { NavigationComponent } from './components/shared/navigation/navigation.component';
 import { ScrollDirective } from './components/shared/navigation/scroll-directive/scroll.directive';
 import { TeamComponent } from './components/shared/team/team.component';
-import { ListOfPetSpeciesComponent } from './components/list-of-pet-species/list-of-pet-species.component';
-import { PetDetailsComponent } from './components/pets/pet-details/pet-details.component';
-import { PetsSectionComponent } from './components/pets/pets-section/pets-section.component';
-import { PetCardComponent } from './components/pets/pet-card/pet-card.component';
 import { ArticlesSectionComponent } from './components/articles/articles-section/articles-section.component';
 import { ArticleComponent } from './components/articles/article/article.component';
 import { BackToTheTopArrowComponent } from './components/shared/back-to-the-top-arrow/back-to-the-top-arrow.component';
@@ -20,6 +16,10 @@ import { VeterinariansSectionComponent } from './components/veterinarians/veteri
 import { VeterinarianCardComponent } from './components/veterinarians/veterinarian-card/veterinarian-card.component';
 import { PetShopsSectionComponent } from './components/pet-shops/pet-shops-section/pet-shops-section.component';
 import { PetShopCardComponent } from './components/pet-shops/pet-shop-card/pet-shop-card.component';
+import { PetDetailsComponent } from './components/pets/pet-details/pet-details.component';
+import { PetsSectionComponent } from './components/pets/pets-section/pets-section.component';
+import { PetCardComponent } from './components/pets/pet-card/pet-card.component';
+import { ListOfPetSpeciesComponent } from './components/pets/list-of-pet-species/list-of-pet-species.component';
 
 @NgModule({
   declarations: [
@@ -44,7 +44,7 @@ import { PetShopCardComponent } from './components/pet-shops/pet-shop-card/pet-s
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
